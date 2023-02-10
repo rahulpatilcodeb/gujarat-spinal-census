5c4327cedb92487943f973383e7cf86e804471df
amarjeet <amarjeetnavik123@gmail.com>
2023-02-10 14:41:24 +0530

    try catch

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -15,33 +15,37 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // console.log(req.headers.authorization);
-  // const verified = jwt.verify(req.headers.authorization, setcretKey);
-  // console.log(verified.email);
-  switch (req.method) {
-    case "GET":
-      const admins = await admin.find({ email: req.body.email });
-      // console.log(admins);
-      // if (verified) {
-      // }
-      // if (admins[0].email != undefined) {
-      const posts = await Contact.find();
-      res.json({
-        data: posts,
-      });
-      // } else {
-      //   res.send({ msg: "not valid user" });
-      // }
-      res.send("Session expired");
-      break;
-    case "POST":
-      const contact = new Contact({
-        contact: req.body.contact,
-        email: req.body.email,
-        description: req.body.description,
-      });
-      await contact.save();
-      // console.log("Contact", contact);
-      return res.send("data saved");
+  try {
+    // console.log(req.headers.authorization);
+    // const verified = jwt.verify(req.headers.authorization, setcretKey);
+    // console.log(verified.email);
+    switch (req.method) {
+      case "GET":
+        const admins = await admin.find({ email: req.body.email });
+        // console.log(admins);
+        // if (verified) {
+        // }
+        // if (admins[0].email != undefined) {
+        const posts = await Contact.find();
+        res.json({
+          data: posts,
+        });
+        // } else {
+        //   res.send({ msg: "not valid user" });
+        // }
+        res.send("Session expired");
+        break;
+      case "POST":
+        const contact = new Contact({
+          contact: req.body.contact,
+          email: req.body.email,
+          description: req.body.description,
+        });
+        await contact.save();
+        // console.log("Contact", contact);
+        return res.send("data saved");
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
