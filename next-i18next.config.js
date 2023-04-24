61c4de280e444217ad814930e093dd48c424259e
Amarjeet <amarjeetnavik123@gmail.com>
2023-04-24 20:26:18 +0530

    language config files

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  i18n: {
+    locales: ["en", "gu"],
+    defaultLocale: "en",
+  },
+  localePath: path.resolve("./public/locales"),
+};
+console.log("path",path.resolve("./public/locales"))
