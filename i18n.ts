8bce0c299e544382ec3d68e36981aed900e484d8
Amarjeet <amarjeetnavik123@gmail.com>
2023-05-03 18:34:31 +0530

    removed i18n.ts file

--- i18n.ts
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   i18:{
-//     locales: ["en", "gu"],    // Array with the languages that you want to use
-//   defaultLocale: "en", // Default language of your website
-//   // pages: {
-//   //   "*": ["common"], // Namespaces that you want to import per page 
-//   // },
-//   }
-// };
