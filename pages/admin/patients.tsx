926dec552a0fe21a02b8ca1be013d1374c23f759
Amarjeet <amarjeetnavik123@gmail.com>
2023-05-05 11:46:01 +0530

    patients page district option updated.

--- pages/admin/patients.tsx
+++ pages/admin/patients.tsx
@@ -183,13 +183,39 @@ export default function Patients() {
                 District
               </option>
               <option value="Ahmedabad">Ahmedabad</option>
-              <option value="Amreli">Amreli</option>
-              <option value="Bharuch">Bharuch</option>
-              <option value="Bhavnagar">Bhavnagar</option>
-              <option value="Jamnagar">Jamnagar</option>
+              <option value="Vadodara">Vadodara</option>
+              <option value="Anand">Anand</option>
+              <option value="Chota Udepur">Chota Udepur</option>
+              <option value="Dahod">Dahod</option>
+              <option value="Kheda">Kheda</option>
+              <option value="Mahisagar">Mahisagar</option>
+              <option value="Panchmahal">Panchmahal</option>
+              <option value="Gandhinagar">Gandhinagar</option>
+              <option value="Aravalli">Aravalli</option>
+              <option value="Banaskantha">Banaskantha</option>
               <option value="Mehsana">Mehsana</option>
-              <option value="Rajkot">Rajkot</option>
+              <option value="Patan">Patan</option>
+              <option value="Sabarkantha">Sabarkantha</option>
+              <option value="Kutch">Kutch</option>
+              <option value="Rajkot">Rajkot</option>s
+              <option value="Amreli">Amreli</option>
+              <option value="Bhavnagar">Bhavnagar</option>
+              <option value="Botad">Botad</option>
+              <option value="Devbhoomi Dwarka">Devbhoomi Dwarka</option>
+              <option value="Gir Somnath">Gir Somnath</option>
+              <option value="Jamnagar">Jamnagar</option>
+              <option value="Junagadh">Junagadh</option>
+              <option value="Morbi">Morbi</option>
+              <option value="Porbandar">Porbandar</option>
+              <option value="Surendranagar">Surendranagar</option>
               <option value="Surat">Surat</option>
+              <option value="Bharuch">Bharuch</option>
+              <option value="Dang">Dang</option>
+              <option value="Narmada">Narmada</option>
+              <option value="Navsari">Navsari</option>
+              <option value="Tapi">Tapi</option>
+              <option value="Valsad">Valsad</option>
+
             </select>
           </div>
         </div>
